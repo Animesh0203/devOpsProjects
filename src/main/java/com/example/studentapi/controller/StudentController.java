950a76340a4042bae338038d15ea7980b795ec6e
Abhimanyu Urs <abhiurs03@gmail.com>
2024-11-16 22:09:28 +0530

    Added changes to create method

--- src/main/java/com/example/studentapi/controller/StudentController.java
+++ src/main/java/com/example/studentapi/controller/StudentController.java
@@ -1,7 +1,7 @@
 package com.example.studentapi.controller;
 
 import java.util.List;
-
+import nothing.com;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
