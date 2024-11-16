d5879c7b689f478d38d1df06a284d0662a1bd610
Abhimanyu Urs <abhiurs03@gmail.com>
2024-11-16 22:05:18 +0530

    Updated student controller

--- src/main/java/com/example/studentapi/controller/StudentController.java
+++ src/main/java/com/example/studentapi/controller/StudentController.java
@@ -1,7 +1,7 @@
 package com.example.studentapi.controller;
 
 import java.util.List;
-
+import java.util.concurrent.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
